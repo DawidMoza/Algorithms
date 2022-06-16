8c7f76631bd629f20199ca0f1d4a45bf0dca4206
DawidMoza <dawidmoza@gmail.com>
2022-06-16 06:01:21 +0200

    depth first search

---
+++ Algorithms/DepthFirstSearch.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms {
+    internal static class DepthFirstSearch {
+
+        internal static bool[] Search(int index, List<int[]> graph, bool[]? visited = null) {
+            if (visited == null) {
+                visited = new bool[graph.Count];
+            }
+            if (!visited[index]) {
+                visited[index] = true;
+                graph[index].Where(x => !visited[x]).ToList().ForEach(x => Search(x, graph, visited));
+            }
+            return visited;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms;
+
+List<int[]> graph = new List<int[]>();
+graph.Add(new int[]{ 1, 4 });
+graph.Add(new int[]{ 0, 2 });
+graph.Add(new int[]{ 0, 1, 3 });
+graph.Add(new int[]{ 0, 2, 4 });
+graph.Add(new int[]{ 0, 3 });
+
+graph.Add(new int[]{ 6 });
+graph.Add(new int[]{ 5, 7, 8 });
+graph.Add(new int[]{ 6 });
+graph.Add(new int[]{ 6 });
+
+bool[] dfs0 = DepthFirstSearch.Search(0, graph);
+bool[] dfs5 = DepthFirstSearch.Search(5, graph);
+
+dfs0.ToList().ForEach(x => Console.Write($"{x} "));
+Console.WriteLine();
+dfs5.ToList().ForEach(x => Console.Write($"{x} "));
